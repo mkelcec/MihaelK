a5bd9a2eafc15ef4637c9483f243434391c207ab
Programsko Inzenjerstvo <pi_foi@outlook.com>
2023-04-24 20:53:53 +0200

    treći pokušaj

--- EvaluationManager12/EvaluationManager12/Form1.Designer.cs
+++ EvaluationManager12/EvaluationManager12/Form1.Designer.cs
@@ -1,5 +1,5 @@
 ﻿namespace EvaluationManager12 {
-    partial class Form1 {
+    partial class FrmLogin {
         /// <summary>
         /// Required designer variable.
         /// </summary>
@@ -23,13 +23,86 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(116, 37);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(100, 20);
+            this.txtUsername.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Korisničko ime:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Lozinka:";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(116, 81);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword.TabIndex = 2;
+            this.txtPassword.UseSystemPasswordChar = true;
+            this.txtPassword.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(125, 136);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Prijava";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // FrmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(302, 203);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUsername);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Prijava";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnLogin;
     }
 }
 
--- EvaluationManager12/EvaluationManager12/Form1.cs
+++ EvaluationManager12/EvaluationManager12/Form1.cs
@@ -9,9 +9,33 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace EvaluationManager12 {
-    public partial class Form1 : Form {
-        public Form1() {
+    public partial class FrmLogin : Form {
+        string username = "nastavnik";
+        string password = "test";
+        public FrmLogin() {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e) {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e) {
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e) {
+            if (txtUsername.Text == "") {
+                MessageBox.Show("Korisnicko ime nije uneseno!", "Problem,", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            } else if (txtPassword.Text == "") {
+                MessageBox.Show("Lozinka nije unesena!", "Problem,", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            } else {
+                if (txtUsername.Text == username && txtPassword.Text == password) {
+                    MessageBox.Show("Dobrodošli", "Prijavljeni ste", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                } else {
+                    MessageBox.Show("Krivi podaci!", "Problem,", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
         }
     }
 }
