d836b31eeb9eb9ee757bd4c7ed98fff0208c3320
Mihael Kelcec <mihaelkelcec@gmail.com>
2023-05-08 18:02:51 +0200

    git commit "Implementacija hijerarhije"

---
+++ EvaluationManager12/EvaluationManager12/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvaluationManager12.Models
+{
+    public class Student : Person
+    {
+        public int Grade
+        {
+            get; set;
+        }
+    }
+}
---
+++ EvaluationManager12/EvaluationManager12/Models/Teacher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvaluationManager12.Models
+{
+    public class Teacher : Person
+    {
+        public string Username
+        {
+            get; set;
+        }
+        public string Password
+        {
+            get; set;
+        }
+    }
+}
