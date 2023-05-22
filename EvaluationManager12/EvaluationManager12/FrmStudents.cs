8a1edaceb6632a3edb83eef78349938de4fb1305
Mihael Kelcec <mihaelkelcec@gmail.com>
2023-05-22 14:02:34 +0200

    "Implementiran prikaz liste studenata"

---
+++ EvaluationManager12/EvaluationManager12/FrmStudents.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EvaluationManager12 {
+    public partial class dgvStudents : Form {
+        public dgvStudents() {
+            InitializeComponent();
+        }
+
+        private void dgvStudents_Load(object sender, EventArgs e) {
+
+        }
+    }
+}
